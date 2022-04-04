553a910f8ad27ddad0cf819f25f171fa801ab9fc
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-04 18:19:42 -0500

    Open menu

---
+++ scripts.js
@@ -0,0 +1,48 @@
+const openMenu = () => {
+  const menuDiv = document.querySelector('#menu-div');
+
+  const menu = document.createElement('div');
+  menu.classList.add('menu');
+  menuDiv.appendChild(menu);
+
+  const closeButton = document.createElement('div');
+  closeButton.classList.add('menu-button');
+  menu.appendChild(closeButton);
+
+  const closeIcon = document.createElement('img');
+  closeIcon.src = 'images/icon-cancel.svg';
+  closeIcon.alt = 'menu icon';
+  closeButton.appendChild(closeIcon);
+
+  const menuList = document.createElement('ul');
+  menuList.classList.add('menu-items');
+  menu.appendChild(menuList);
+
+  let menuItem = document.createElement('li');
+  menuItem.classList.add('menu-item');
+  let anchor = document.createElement('a');
+  anchor.href = '#works';
+  anchor.textContent = 'Portfolio';
+  menuItem.appendChild(anchor);
+  menuList.appendChild(menuItem);
+
+  menuItem = document.createElement('li');
+  menuItem.classList.add('menu-item');
+  anchor = document.createElement('a');
+  anchor.href = '#about';
+  anchor.textContent = 'About';
+  menuItem.appendChild(anchor);
+  menuList.appendChild(menuItem);
+
+  menuItem = document.createElement('li');
+  menuItem.classList.add('menu-item');
+  anchor = document.createElement('a');
+  anchor.href = '#contact';
+  anchor.textContent = 'Contact';
+  menuItem.appendChild(anchor);
+  menuList.appendChild(menuItem);
+};
+
+document.querySelector('.menu-button').addEventListener('click', () => {
+  openMenu();
+});
